829788f5f9eb9a38425161112ed9635e1d457803
Ty Day <tyrday@gmail.com>
2020-05-28 13:41:10 -0400

    block google bots

--- src/components/seo.js
+++ src/components/seo.js
@@ -36,6 +36,10 @@ function SEO({ description, lang, meta, title }) {
       titleTemplate={`%s | ${site.siteMetadata.title}`}
       meta={[
         {
+          name: `robots`,
+          content: 'none',
+        },
+        {
           name: `description`,
           content: metaDescription,
         },
